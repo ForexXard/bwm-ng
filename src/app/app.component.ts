3734cb7a2ef75ccec039f1cb418ffb100f9c235d
ForexXard <forex.xard@gmail.com>
2018-09-15 10:27:10 -0700

    error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  component = "i am a component value"
+
+  clickHandler(){
+    alert("i am clicked")
+  }
 }
